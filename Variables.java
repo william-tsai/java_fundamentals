5cf4409c1215b3f1bf728f7413583668fbaa7110
William_Tsai <tsaiyuhlin@gmail.com>
2018-09-18 02:43:44 -0700

    Added ImportDemo and ImportDemoTest to learn importing java classes

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String [] args) {
+        int ourInt;
+        ourInt = 400;
+        double pi = 3.1415;
+        boolean bool = true;
+        char singleCharacter = "A";
+        String multipleCharacters = "ABC";
+    }
+}
