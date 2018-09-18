a799cd48dbb158c294349a4de7901150bef149c8
William_Tsai <tsaiyuhlin@gmail.com>
2018-09-18 01:43:24 -0700

    Added SelfIntroduction

---
+++ SelfIntroduction.java
@@ -0,0 +1,7 @@
+public class SelfIntroduction {
+    public static void main(String [] args) {
+        System.out.println("My name is William Tsai.");
+        System.out.println("I am 32 years old.");
+        System.out.println("My hometown is Taipei, Taiwan.");
+    }
+}
