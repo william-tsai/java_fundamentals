3a0982c687b3f6e06462d5af65900711d46e541f
William_Tsai <tsaiyuhlin@gmail.com>
2018-09-20 01:40:21 -0700

    Added FizzBuzz strings and conditionals exercise

---
+++ FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+    public String fizzbuzz(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else if (number % 3 == 0){
+            return "Fizz";
+        } else {
+            return String.format("%d", number);
+        }
+    }
+}
---
+++ FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz numberTest = new FizzBuzz();
+        System.out.println(numberTest.fizzbuzz(3));
+        System.out.println(numberTest.fizzbuzz(5));
+        System.out.println(numberTest.fizzbuzz(15));
+        System.out.println(numberTest.fizzbuzz(2));
+    }
+}
